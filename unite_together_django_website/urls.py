74e9eeceb0811835f10224e9f1bc9951cbe48ae6
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-14 21:43:10 +0400

    added: urls "home"

--- unite_together_django_website/urls.py
+++ unite_together_django_website/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from unite_together_django_website import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
 ]
