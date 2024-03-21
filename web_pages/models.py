a9e3ad0c19bf54ac0001f599e7a4a3185b5f5fe7
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-21 12:00:36 +0400

    added: data models WebPage, WebContentObject, WebPageSection

---
+++ web_pages/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class WebContentObject(models.Model):
+    name = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, blank=True)
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='img/pages_content', blank=True)
+        
+    subordinate_objects = models.ManyToManyField('self', symmetrical=True, blank=True)
+    
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.title} - {self.text}"
+
+
+class WebPage(models.Model):
+    name = models.CharField(max_length=250)
+
+    # friends = models.ManyToManyField('self', symmetrical=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WebPageSection(models.Model):
+    name = models.CharField(max_length=250)
+    page = models.ForeignKey(WebPage, on_delete=models.CASCADE)
+    content = models.ForeignKey(WebContentObject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.page} - {self.content}"
