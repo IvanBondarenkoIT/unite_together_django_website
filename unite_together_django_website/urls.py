b5cdeb113a2183393c6669e410ae3f89e4d756d6
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-22 09:53:21 +0400

    added: media files settings

--- unite_together_django_website/urls.py
+++ unite_together_django_website/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls.static import static
 from unite_together_django_website import views
+from unite_together_django_website import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
