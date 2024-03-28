6a2fdd185174a16d353595922119d9d506542734
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-28 22:14:11 +0400

    added: linc navigation aboutUs, register, login pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,8 +7,8 @@ from .models import Account
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
-    readonly_fields = ('date_joined', 'last_login')
-    ordering = ('-date_joined',)
+    # readonly_fields = ('date_joined', 'last_login')
+    # ordering = ('-date_joined',)
 
     filter_horizontal = ()
     list_filter = ()
