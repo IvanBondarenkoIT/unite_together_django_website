355d0bd9446c9df7a54bff1c29b1ada32d61d3f4
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-29 15:46:46 +0400

    added: account activation, encode user PK, send token based link

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from accounts.forms import RegistrationForm
 from accounts.models import Account
@@ -28,6 +34,20 @@ def register(request):
 
             user.phone_number = phone_number
             user.save()
+
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = "Please activate your account"
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
 
             messages.success(request, "Registration Successful")
             return redirect("register")
