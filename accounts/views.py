e9e9c4198bdd2fc4c859ee2b3d125c789c8854d9
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-03-29 15:52:02 +0400

    added: account activation, decode user PK and activate the user

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from accounts.forms import RegistrationForm
 from accounts.models import Account
@@ -49,8 +49,8 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            messages.success(request, "Registration Successful")
-            return redirect("register")
+            # messages.success(request, "Registration Successful")
+            return redirect(f"/accounts/login/?command=verification&email={email}")
 
     else:
         form = RegistrationForm()
@@ -84,3 +84,21 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "Logout Successful")
     return redirect("login")
+
+
+def activate(request, uidb64, token):
+    # return HttpResponse("OK")
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Congratulations! Your account has been activated!")
+        return redirect("login")
+    else:
+        messages.error(request, "Invalid activation link")
+        return redirect("Register")
