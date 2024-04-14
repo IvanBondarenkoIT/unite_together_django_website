d14edd2bdc73645ce6be67395e6833d44bbc91be
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-04-14 15:32:19 +0400

    added: allowed_hosts

--- unite_together_django_website/settings.py
+++ unite_together_django_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = ["http://django-unite-together-env.eba-brndinwr.us-west-2.elasticbeanstalk.com/",
+ALLOWED_HOSTS = ["'django-unite-together-env.eba-brndinwr.us-west-2.elasticbeanstalk.com'",
                  "127.0.0.1",
                  ]
 
