7dc50781c6f3d3ecf35843d6df0e5a78af9545c2
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-05-15 12:47:27 +0400

    added: home page nav-links

--- web_pages/models.py
+++ web_pages/models.py
@@ -94,11 +94,11 @@ class Projects(WebContentObject):
     # address = models.CharField(max_length=250, blank=True, null=True)
     # start_age = models.IntegerField(blank=True, null=True)
     # end_age = models.IntegerField(blank=True, null=True)
-    start_date = models.DateField(blank=True,null=True)
-    end_date = models.DateField(blank=True,null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        return f"Event - {self.name}"
+        return f"Project - {self.name}"
 
     def get_start_date_year(self):
         return self.start_date.strftime("%Y")
@@ -110,4 +110,4 @@ class Projects(WebContentObject):
         return self.start_date.strftime("%d")
 
     def get_url(self):
-        return reverse("event_detail", args=[self.group.slug, self.slug])
+        return reverse("project_detail", args=[self.group.slug, self.slug])
