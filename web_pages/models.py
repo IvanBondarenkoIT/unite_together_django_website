ed6c7a202b1a4693c87176a11dbf0cdb1985c6ee
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-06-17 11:55:57 +0400

    added: Initiative model blank=True, null=True

--- web_pages/models.py
+++ web_pages/models.py
@@ -146,11 +146,11 @@ class ProjectGallery(models.Model):
 
 
 class Initiative(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='initiatives/')
-    order = models.IntegerField(default=0)
-    link = models.URLField()
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='initiatives/', blank=True, null=True)
+    order = models.IntegerField(default=0, blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
