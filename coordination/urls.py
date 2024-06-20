c1519c2e63bde98347f9ca2e6d0a69842f9a1e13
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-06-20 11:26:48 +0400

    added: Coordination events list is done

--- coordination/urls.py
+++ coordination/urls.py
@@ -6,15 +6,17 @@ from .views import (
 )
 
 urlpatterns = [
-    path('events/', event_list, name='event_list'),
+    path('events-list/', event_list, name='event_list'),
     path('events/create/', event_create, name='event_create'),
     path('events/<int:pk>/update/', event_update, name='event_update'),
     path('events/<int:pk>/delete/', event_delete, name='event_delete'),
-    path('participants/', participant_list, name='participant_list'),
+
+    path('participants/<int:pk>', participant_list, name='participant_list'),
     path('participants/create/', participant_create, name='participant_create'),
     path('participants/<int:pk>/update/', participant_update, name='participant_update'),
     path('participants/<int:pk>/delete/', participant_delete, name='participant_delete'),
-    path('persons/', person_list, name='person_list'),
+
+    path('persons-list/', person_list, name='coordination_person_list'),
     path('persons/create/', person_create, name='person_create'),
     path('persons/<int:pk>/update/', person_update, name='person_update'),
     path('persons/<int:pk>/delete/', person_delete, name='person_delete'),
--- coordination/views.py
+++ coordination/views.py
@@ -46,8 +46,12 @@ def event_delete(request, pk):
 
 @login_required
 # @permission_required('web_pages.view_participant', raise_exception=True)
-def participant_list(request):
-    participants = Participant.objects.all()
+def participant_list(request, pk):
+    if pk:
+        event = get_object_or_404(Events, pk=pk)
+        participants = Participant.objects.all().filter(is_active=True, registered_on=event)
+    else:
+        participants = Participant.objects.all()
     return render(request, 'coordination/participant_list.html', {'participants': participants})
 
 @login_required
