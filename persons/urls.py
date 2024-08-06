dacb26f4eecd86b36091794371be9a6f3047aa6e
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-08-06 12:27:56 +0400

    added: personal account persons list total rebuild. done with edit and create new person

--- persons/urls.py
+++ persons/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from . import views
-from .views import create_person, person_list
+
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.associated_person_list, name='dashboard'),
 
-    path('associated_persons/', views.associated_person_list, name='associated_person_list'),
-    path('associated_persons/create/', views.associated_person_create, name='associated_person_create'),
-    path('associated_persons/<int:pk>/edit/', views.associated_person_edit, name='associated_person_edit'),
+    path('associated-persons/', views.associated_person_list, name='associated_person_list'),
+    path('associated-persons/create/', views.associated_person_create, name='associated_person_create'),
+    path('associated-persons/<int:pk>/edit/', views.associated_person_edit, name='associated_person_edit'),
 
 
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('create-person/', views.create_person, name='create_person'),
-    path('persons-list/', views.person_list, name='person_list'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
+    # path('create-person/', views.create_person, name='create_person'),
+    # path('persons-list/', views.person_list, name='person_list'),
 
     # path('create-participant/', views.create_participant, name='create_participant'),
     # path('participants-list/', views.participant_list, name='participant_list'),
