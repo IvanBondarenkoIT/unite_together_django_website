9fb1d1286d3d02339d4d6a6efa5655dbfb11eb67
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-08-14 22:34:32 +0400

    added: default CitizenshipChoices - UKRAINIAN

--- persons/models.py
+++ persons/models.py
@@ -49,7 +49,7 @@ class Person(models.Model):
     citizenship = models.CharField(
         max_length=20,
         choices=CitizenshipChoices.choices,
-        default=CitizenshipChoices.GEORGIAN,
+        default=CitizenshipChoices.UKRAINIAN,
         blank=True,
         null=True
     )
