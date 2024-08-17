b41e2be06ea6adfbb9d84616a5d7f702c5f6fd4c
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-08-17 18:22:58 +0400

    added: translate homepage-desktop, homepage-mobile

--- web_pages/context_processors.py
+++ web_pages/context_processors.py
@@ -9,31 +9,33 @@ def generate_url(group_name, slug):
 
 
 def menu_links(request):
-    if '/about-us/' in request.path:
+    if "/about-us/" in request.path:
         links = {
-            "Who we are": "who_we_are",
-            "Documents": "documents",
-            "Partners and collaboration": "partners",
-            "Contacts": "contact",
+            "Про нас": "who_we_are",
+            "Документи": "documents",
+            "Партнери та співпраця": "partners",
+            "Контакти": "contact",
         }
         return {"links": links}
 
     page_name = None
 
-    if '/events/' in request.path:
+    if "/events/" in request.path:
         page_name = "events"
-    elif '/projects/' in request.path:
+    elif "/projects/" in request.path:
         page_name = "projects"
 
     if page_name:
-        links = ObjectsGroup.objects.filter(page__name=page_name).order_by("order").annotate(
-            pre_computed_url=generate_url(page_name, F('slug'))
+        links = (
+            ObjectsGroup.objects.filter(page__name=page_name)
+            .order_by("order")
+            .annotate(pre_computed_url=generate_url(page_name, F("slug")))
         )
     else:
-        links = ObjectsGroup.objects.all().order_by("order").annotate(
-            pre_computed_url=generate_url(page_name, F('slug'))
+        links = (
+            ObjectsGroup.objects.all()
+            .order_by("order")
+            .annotate(pre_computed_url=generate_url(page_name, F("slug")))
         )
-    print(links)
+
     return {"links": links}
-
-
