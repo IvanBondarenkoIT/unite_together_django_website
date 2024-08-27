f45021434cf70f0a4dfc6d631bab0c51145470b7
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-08-27 22:56:09 +0400

    added: Persons criteria A-E

--- persons/forms.py
+++ persons/forms.py
@@ -9,66 +9,138 @@ from .models import AssociatedPerson, Participant
 class AssociatedPersonAdminForm(forms.ModelForm):
     class Meta:
         model = AssociatedPerson
-        fields = '__all__'
+        fields = "__all__"
         # widgets = {
         #     'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         #     'date_of_arrival': forms.DateInput(attrs={'type': 'date'}),
         # }
 
+    CRITERIA_CHOICES = [
+        (
+            "A",
+            "Тяжко хворі, з інвалідністю чи обмеженими можливостями (з наявними в документами)",
+        ),
+        ("B", "Старше 60 років (за датою народження)"),
+        ("C", "Багатодітна родина (3 та більше дитини до 18 років)"),
+        ("D", "Одинока мати/батько, що самостійно виховує неповнолітніх дітей"),
+        ("E", "Жоден з вищеперечислених критеріїв не підходить"),
+    ]
+
+    user_selected_criteria = forms.ChoiceField(
+        choices=CRITERIA_CHOICES, required=False, label="Критерій (Виберіть вручну)"
+    )
+
 
 class AssociatedPersonForm(forms.ModelForm):
+
     class Meta:
+        CRITERIA_CHOICES = [
+            (
+                "A",
+                "Тяжко хворі, з інвалідністю чи обмеженими можливостями (з наявними в документами)",
+            ),
+            ("B", "Старше 60 років (за датою народження)"),
+            ("C", "Багатодітна родина (3 та більше дитини до 18 років)"),
+            ("D", "Одинока мати/батько, що самостійно виховує неповнолітніх дітей"),
+            ("E", "Жоден з вищеперечислених критеріїв не підходить"),
+        ]
         model = AssociatedPerson
         fields = [
-            'first_name', 'last_name', 'date_of_birth', 'citizenship', 'date_of_arrival',
-            'type_of_document', 'document_number', 'gender', 'georgian_phone_number',
-            'ukrainian_phone_number', 'country', 'chosen_city', 'address_line', 'is_active'
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "citizenship",
+            "date_of_arrival",
+            "type_of_document",
+            "document_number",
+            "gender",
+            "georgian_phone_number",
+            "ukrainian_phone_number",
+            "country",
+            "chosen_city",
+            "address_line",
+            "is_active",
         ]
         widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date', 'placeholder': 'ДД.ММ.РРРР'}),
-            'date_of_arrival': forms.DateInput(attrs={'type': 'date', 'placeholder': 'ДД.ММ.РРРР'}),
-            'first_name': forms.TextInput(attrs={'placeholder': 'Ім\'я', 'required': True}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Прізвище', 'required': True}),
-            'citizenship': forms.Select(attrs={
-                'placeholder': 'Громадянство',
-                'class': 'form-control',
-                'required': True,
-            }),
-            'type_of_document': forms.Select(attrs={
-                'placeholder': 'Тип документа',
-                'class': 'form-control',
-                'required': True,
-            }),
-            'document_number': forms.TextInput(attrs={'placeholder': 'Номер документа', 'required': True}),
-            'georgian_phone_number': forms.TextInput(attrs={
-                'placeholder': 'Грузинський номер телефону',
-                'pattern': r'^\995[0-9]{9}$',
-                'required': True,
-            }),
-            'ukrainian_phone_number': forms.TextInput(attrs={
-                'placeholder': 'Український номер телефону',
-                'pattern': r'^\380[0-9]{9}$'
-            }),
-            'address_line': forms.TextInput(attrs={'placeholder': 'Адреса', 'required': True}),
+            "date_of_birth": forms.DateInput(
+                attrs={"type": "date", "placeholder": "ДД.ММ.РРРР"}
+            ),
+            "date_of_arrival": forms.DateInput(
+                attrs={"type": "date", "placeholder": "ДД.ММ.РРРР"}
+            ),
+            "first_name": forms.TextInput(
+                attrs={"placeholder": "Ім'я", "required": True}
+            ),
+            "last_name": forms.TextInput(
+                attrs={"placeholder": "Прізвище", "required": True}
+            ),
+            "citizenship": forms.Select(
+                attrs={
+                    "placeholder": "Громадянство",
+                    "class": "form-control",
+                    "required": True,
+                }
+            ),
+            "type_of_document": forms.Select(
+                attrs={
+                    "placeholder": "Тип документа",
+                    "class": "form-control",
+                    "required": True,
+                }
+            ),
+            "document_number": forms.TextInput(
+                attrs={"placeholder": "Номер документа", "required": True}
+            ),
+            "georgian_phone_number": forms.TextInput(
+                attrs={
+                    "placeholder": "Грузинський номер телефону",
+                    "pattern": r"^\995[0-9]{9}$",
+                    "required": True,
+                }
+            ),
+            "ukrainian_phone_number": forms.TextInput(
+                attrs={
+                    "placeholder": "Український номер телефону",
+                    "pattern": r"^\380[0-9]{9}$",
+                }
+            ),
+            "address_line": forms.TextInput(
+                attrs={"placeholder": "Адреса", "required": True}
+            ),
+            # add criteria
+            "user_selected_criteria": forms.ChoiceField(
+                choices=CRITERIA_CHOICES,
+                required=False,
+                label="Критерій (Виберіть вручну)",
+            ),
+            #
         }
 
+        # user_selected_criteria = forms.ChoiceField(
+        #     choices=CRITERIA_CHOICES, required=False, label="Критерій (Виберіть вручну)"
+        # )
+
     def __init__(self, *args, **kwargs):
-        default_ge_phone = kwargs.pop('default_ge_phone', None)
+        default_ge_phone = kwargs.pop("default_ge_phone", None)
         super().__init__(*args, **kwargs)
         if default_ge_phone:
-            self.fields['georgian_phone_number'].initial = default_ge_phone
+            self.fields["georgian_phone_number"].initial = default_ge_phone
 
         # Перетворення формату дати для відображення у формі
         if self.instance.pk:  # Якщо об'єкт вже існує
             if self.instance.date_of_birth:
-                self.fields['date_of_birth'].initial = self.instance.date_of_birth.strftime('%d.%m.%Y')
+                self.fields["date_of_birth"].initial = (
+                    self.instance.date_of_birth.strftime("%d.%m.%Y")
+                )
             if self.instance.date_of_arrival:
-                self.fields['date_of_arrival'].initial = self.instance.date_of_arrival.strftime('%d.%m.%Y')
+                self.fields["date_of_arrival"].initial = (
+                    self.instance.date_of_arrival.strftime("%d.%m.%Y")
+                )
 
     def clean_document_number(self):
-        type_of_document = self.cleaned_data.get('type_of_document')
+        type_of_document = self.cleaned_data.get("type_of_document")
         if type_of_document:
-            document_number = self.cleaned_data.get('document_number')
+            document_number = self.cleaned_data.get("document_number")
             pattern = type_of_document.regex
             if document_number and not re.match(pattern, document_number):
                 raise forms.ValidationError(
@@ -79,57 +151,67 @@ class AssociatedPersonForm(forms.ModelForm):
             raise forms.ValidationError("Необхідно вибрати тип документа")
 
     def clean_first_name(self):
-        first_name = self.cleaned_data.get('first_name')
+        first_name = self.cleaned_data.get("first_name")
         if not first_name:
             raise forms.ValidationError("Ім'я є обов'язковим.")
         return first_name
 
     def clean_last_name(self):
-        last_name = self.cleaned_data.get('last_name')
+        last_name = self.cleaned_data.get("last_name")
         if not last_name:
             raise forms.ValidationError("Прізвище є обов'язковим.")
         return last_name
 
     def clean_georgian_phone_number(self):
-        georgian_phone_number = self.cleaned_data.get('georgian_phone_number')
+        georgian_phone_number = self.cleaned_data.get("georgian_phone_number")
         if georgian_phone_number:
-            pattern = r'^995[0-9]{9}$'
+            pattern = r"^995[0-9]{9}$"
             if not re.match(pattern, georgian_phone_number):
-                raise forms.ValidationError("Грузинський номер телефону має бути у форматі: 995XXXXXXXXX.")
+                raise forms.ValidationError(
+                    "Грузинський номер телефону має бути у форматі: 995XXXXXXXXX."
+                )
             return georgian_phone_number
         else:
             raise forms.ValidationError("Грузинський номер телефону є обов'язковим.")
 
     def clean_ukrainian_phone_number(self):
-        ukrainian_phone_number = self.cleaned_data.get('ukrainian_phone_number')
+        ukrainian_phone_number = self.cleaned_data.get("ukrainian_phone_number")
         if ukrainian_phone_number:
-            pattern = r'^380[0-9]{9}$'
+            pattern = r"^380[0-9]{9}$"
             if not re.match(pattern, ukrainian_phone_number):
-                raise forms.ValidationError("Український номер телефону має бути у форматі: 380XXXXXXXXX.")
+                raise forms.ValidationError(
+                    "Український номер телефону має бути у форматі: 380XXXXXXXXX."
+                )
             return ukrainian_phone_number
         return ukrainian_phone_number  # Відсутня помилка перевірки, якщо поле залишено порожнім
 
     def clean_date_of_birth(self):
-        date_of_birth = self.cleaned_data.get('date_of_birth')
+        date_of_birth = self.cleaned_data.get("date_of_birth")
         if not date_of_birth:
-            raise ValidationError("Дата народження є обов'язковою і повинна бути у форматі ДД.ММ.РРРР.")
+            raise ValidationError(
+                "Дата народження є обов'язковою і повинна бути у форматі ДД.ММ.РРРР."
+            )
         return date_of_birth
 
     def clean_date_of_arrival(self):
-        date_of_arrival = self.cleaned_data.get('date_of_arrival')
+        date_of_arrival = self.cleaned_data.get("date_of_arrival")
         if not date_of_arrival:
-            raise ValidationError("Дата прибуття є обов'язковою і повинна бути у форматі ДД.ММ.РРРР.")
+            raise ValidationError(
+                "Дата прибуття є обов'язковою і повинна бути у форматі ДД.ММ.РРРР."
+            )
         return date_of_arrival
 
 
 class ParticipantAdminForm(forms.ModelForm):
     class Meta:
         model = Participant
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
-            'date_of_arrival': forms.DateInput(attrs={'type': 'date'}),
+            "date_of_birth": forms.DateInput(attrs={"type": "date"}),
+            "date_of_arrival": forms.DateInput(attrs={"type": "date"}),
         }
 
 
-AssociatedPersonFormSet = modelformset_factory(AssociatedPerson, form=AssociatedPersonForm, extra=0)
+AssociatedPersonFormSet = modelformset_factory(
+    AssociatedPerson, form=AssociatedPersonForm, extra=0
+)
