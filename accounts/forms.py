46217e494193377269070a6155d7ca2ace7a08b4
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-09-16 17:56:37 +0400

    added: registration form all attributes validation included.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 from unite_together_django_website.validators import CustomPasswordValidator
@@ -12,7 +13,7 @@ class RegistrationForm(forms.ModelForm):
                 "placeholder": "Введіть пароль",
                 "class": "form-control",
             }
-        )
+        ),
     )
 
     confirm_password = forms.CharField(
@@ -31,9 +32,13 @@ class RegistrationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields["first_name"].widget.attrs["placeholder"] = "Ім'я"
+        self.fields["first_name"].widget.attrs["required"] = True
         self.fields["last_name"].widget.attrs["placeholder"] = "Прізвище"
+        self.fields["last_name"].widget.attrs["required"] = True
         self.fields["phone_number"].widget.attrs["placeholder"] = "Номер телефону"
+        self.fields["phone_number"].widget.attrs["required"] = True
         self.fields["email"].widget.attrs["placeholder"] = "Електрона адреса"
+        self.fields["email"].widget.attrs["required"] = True
         for field in self.fields:
             self.fields[field].widget.attrs["class"] = "form-control"
 
@@ -49,3 +54,26 @@ class RegistrationForm(forms.ModelForm):
 
         CustomPasswordValidator().validate(password)
 
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+        if not first_name:
+            raise forms.ValidationError("Ім'я є обов'язковим.")
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get("last_name")
+        if not last_name:
+            raise forms.ValidationError("Прізвище є обов'язковим.")
+        return last_name
+
+    def clean_georgian_phone_number(self):
+        georgian_phone_number = self.cleaned_data.get("georgian_phone_number")
+        if georgian_phone_number:
+            pattern = r"^995[0-9]{9}$"
+            if not re.match(pattern, georgian_phone_number):
+                raise forms.ValidationError(
+                    "Грузинський номер телефону має бути у форматі: 995XXXXXXXXX."
+                )
+            return georgian_phone_number
+        else:
+            raise forms.ValidationError("Грузинський номер телефону є обов'язковим.")
